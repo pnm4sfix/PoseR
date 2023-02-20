2d3412ebe26faeedce8052f778ba7ebf7b515218
pnm4sfix <45112367+pnm4sfix@users.noreply.github.com>
2023-02-20 17:14:48 +0000

    changed imports

--- src/poser/__init__.py
+++ src/poser/__init__.py
@@ -3,5 +3,9 @@ try:
 except ImportError:
     __version__ = "unknown"
 
+from poser._widget import PoserWidget
+from poser._loader import HyperParams
+from poser._loader import ZebData
+from poser.utils import Animation
 
 __all__ = "PoserWidget"
